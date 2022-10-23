58d2f25addb8e5a116b3e67074e0a369ba410b54
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2022-10-23 18:32:36 +0300

    Init commit

---
+++ src/test/java/ru/kata/academy/kovtunenko/third/block/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.kata.academy.kovtunenko.third.block;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
