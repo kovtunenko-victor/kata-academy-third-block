71a5589ed072a590cb0ece757ad16015dc677164
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2022-11-05 18:36:29 +0300

    Do task

--- src/main/java/ru/kata/academy/kovtunenko/third/block/controller/UserController.java
+++ src/main/java/ru/kata/academy/kovtunenko/third/block/controller/UserController.java
@@ -2,14 +2,8 @@ package ru.kata.academy.kovtunenko.third.block.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import ru.kata.academy.kovtunenko.third.block.model.User;
-import ru.kata.academy.kovtunenko.third.block.service.RoleService;
 import ru.kata.academy.kovtunenko.third.block.service.UserService;
 import java.security.Principal;
 
@@ -17,11 +11,9 @@ import java.security.Principal;
 public class UserController {
 
     private final UserService userService;
-    private final RoleService roleService;
 
-    public UserController(UserService service, RoleService roleService) {
+    public UserController(UserService service) {
         this.userService = service;
-        this.roleService = roleService;
     }
 
     @GetMapping("/user/details")
@@ -34,11 +26,5 @@ public class UserController {
     public String printUser(ModelMap model, Principal principal) {
         model.addAttribute("user", (User)userService.loadUserByUsername(principal.getName()));
         return "userApp/mainPage";
-    }
-
-    @DeleteMapping(value = "/admin/users/delete/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        userService.deleteById(id);
-        return "redirect:/admin/users";
     }
 }
